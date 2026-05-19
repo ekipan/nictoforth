74f2b60a642157d19bbcb73154da0d2c0cc9c678
ekipan <ekipan@users.noreply.github.com>
2026-05-19 10:56:59 -0400

    concretize more byte costs

--- nicto.asm
+++ nicto.asm
@@ -361,7 +361,7 @@ execute: ; execute ( ... xt -- ... )
 ; happen to collide!
 
 ; [5c] could reuse from forth if the flags were taken
-; from the nt on the stack. costs instructions though.
+; from the nt on the stack, but it costs 3 bytes.
 ; sectorforth has a *wildly* dense dispatch routine I
 ; adore. sadly this simple one costs the same and has
 ; a milder gotcha: STATE high byte is ignored.
@@ -400,8 +400,8 @@ quit:   ; quit ( -- ) everything else, then loop.
 ; [6a] apparently setting ss disables interrupts briefly
 ; so it makes the sp load safer. sure, I'll have it.
 
-; [6b] MAIN vector enables runtime interpreter swapping:
-; define a new interpeter in forth, reuse all the pieces
+; [6b] vectored MAIN costs 4 bytes, enabling interpreter
+; hotswap: define a new interpreter in forth, reuse
 ; `line lex find abort c.call execute`, maybe add number
 ; parsing or whatever, then store it into MAIN and it
 ; becomes the new main loop:  ' my-interpret MAIN !
