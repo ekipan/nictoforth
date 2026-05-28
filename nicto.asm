ekipan/nictoforth

commit ca35dd0e5e5d2d4a1361f713d89753fc5fdbf19a

decouple missing directly from number

nicto.asm
@@ -304,8 +304,10 @@ lex:    ; lex ( "name" -- addr len ) parse a word.
 ; could also recover standard `parse-name` after
 ; defining `+!` and `1`:   : parse-name lex 1 >in +! ;
 
-%if 0 ; 26 bytes. immediates only, no lit.
-number: ; >number ( addr len 0 == n ) no error check.
+%if 0 ; 27 bytes. immediates only, no lit.
+missing:
+        INC2 bp         ; ( addr len 0 ) drop
+number: ; >number ( addr len -/- n ) no error check.
         mov si,W[bp+4]
         mov cx,W[bp+2]
         xor dx,dx
@@ -316,10 +318,8 @@ number: ; >number ( addr len 0 == n ) no error check.
         add dx,ax
         loop .digit
         xchg ax,dx      ; 1 byte [1a].
-        add bp,4
+        INC2 bp
         jmp putax
-
-missing equ number
 %else
 missing equ error       ; might correct underflow [5c].
 %endif

commit b16e80dd78ae1235b7b274a7eb8c387b7534647f

fix number
Whoops, broken by ca35dd0e5e5d2d4a1361f713d89753fc5fdbf19a

nicto.asm
@@ -308,8 +308,8 @@ lex:    ; lex ( "name" -- addr len ) parse a word.
 missing:
         INC2 bp         ; ( addr len 0 ) drop
 number: ; >number ( addr len -/- n ) no error check.
-        mov si,W[bp+4]
-        mov cx,W[bp+2]
+        mov si,W[bp+2]
+        mov cx,W[bp+0]
         xor dx,dx
 .digit: lodsb
         cbw             ; ah = 0 (or 0xff).